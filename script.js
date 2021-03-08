0f237efe3f9600e37835c835aaf1cb18ada537e9
tod619 <trevorodwyer619@hotmail.com>
2021-03-08 14:37:02 +0000

    add style to btn class in style.css file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sounds = ["applause", "boo", "gasp","tada", "victory", "wrong"]
 
 sounds.forEach(sound => {
     const btn = document.createElement("button")
-    btn.classList.add(".btn")
+    btn.classList.add("btn")
 
     btn.innerText = sound
 
